2b3ede8749bb525eb0f6de00f481dea99b5f0f49
pallandir <pallandir2@gmail.com>
2025-03-05 14:21:46 +0800

    Add defaults layouts

---
+++ src/layouts/Dashboard.vue
@@ -0,0 +1,18 @@
+<template>
+  <aside class="max-w-52 h-full">
+    <AppSidebar />
+  </aside>
+  <main class="w-[calc(100%-13rem)] h-full">
+    <section class="h-11 min-h-11pb-2">
+      <AppBreadcrumb /><Divider class="m-px" />
+    </section>
+    <section class="h-[calc(100%-2.75rem-2px)] bg-gray-100 mt-0.5 p-2">
+      <slot></slot>
+    </section>
+  </main>
+</template>
+
+<script setup lang="ts">
+import AppSidebar from "@/components/Navigation/AppSidebar.vue";
+import AppBreadcrumb from "@/components/Navigation/AppBreadcrumb.vue";
+</script>
